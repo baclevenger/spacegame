689614a1992c1ef4d5459a256686bb1a1bb1f6b7
Blake Clevenger <clevengb@gmail.com>
2015-03-16 16:56:41 -0400

    changes

--- Site/models/Station.js
+++ Site/models/Station.js
@@ -9,116 +9,122 @@ var stationSchema = mongoose.Schema({
         Y: Number,
         Z: Number
     },
-level:{
-        0: {
+    level:{
+            0: {
 
-        },   
+            },   
         
-        1: {
-            slot1: {
-                installation: mongoose.Schema.ObjectId,
-            },
-            slot2: {
-                installation: mongoose.Schema.ObjectId, 
-            },
-            slot3: {
-                installation: mongoose.Schema.ObjectId,
-            },
-            slot4: {
-                installation: mongoose.Schema.ObjectId,
-            },
-            slot5: {
-                installation: mongoose.Schema.ObjectId,
-            },
-            slot6: {
-                installation: mongoose.Schema.ObjectId,
-            },
-            slot7: {
-                installation: mongoose.Schema.ObjectId,
-            },
-            slot8: {
-                installation: mongoose.Schema.ObjectId,
-            }
-        },
-
-    2: {
-    slot1: {
-        installation: mongoose.Schema.ObjectId,
+            1: {
+                slot1: {
+                    installation: mongoose.Schema.ObjectId,
                 },
-    slot2: {
-        installation: mongoose.Schema.ObjectId, 
+                slot2: {
+                    installation: mongoose.Schema.ObjectId, 
                 },
-    slot3: {
-        installation: mongoose.Schema.ObjectId,
+                slot3: {
+                    installation: mongoose.Schema.ObjectId,
                 },
-    slot4: {
-        installation: mongoose.Schema.ObjectId,
+                slot4: {
+                    installation: mongoose.Schema.ObjectId,
                 },
-    slot5: {
-        installation: mongoose.Schema.ObjectId,
+                slot5: {
+                    installation: mongoose.Schema.ObjectId,
                 },
-    slot6: {
-        installation: mongoose.Schema.ObjectId,
+                slot6: {
+                    installation: mongoose.Schema.ObjectId,
                 },
-    slot7: {
-        installation: mongoose.Schema.ObjectId,
+                slot7: {
+                    installation: mongoose.Schema.ObjectId,
                 },
-    slot8: {
-        installation: mongoose.Schema.ObjectId,
-            },
-    slot9: {
-         installation: mongoose.Schema.ObjectId,
-                },
-    slot10: {
-        installation: mongoose.Schema.ObjectId,
+                slot8: {
+                    installation: mongoose.Schema.ObjectId,
                 }
-        },
+            },
 
-        3: {
+            2: {
             slot1: {
                 installation: mongoose.Schema.ObjectId,
-            },
+                        },
             slot2: {
                 installation: mongoose.Schema.ObjectId, 
-            },
+                        },
             slot3: {
                 installation: mongoose.Schema.ObjectId,
-            },
+                        },
             slot4: {
                 installation: mongoose.Schema.ObjectId,
-            },
+                        },
             slot5: {
                 installation: mongoose.Schema.ObjectId,
-            },
+                        },
             slot6: {
                 installation: mongoose.Schema.ObjectId,
-            },
+                        },
             slot7: {
                 installation: mongoose.Schema.ObjectId,
-            },
+                        },
             slot8: {
                 installation: mongoose.Schema.ObjectId,
-            },
+                    },
             slot9: {
-                installation: mongoose.Schema.ObjectId,
-            },
+                 installation: mongoose.Schema.ObjectId,
+                        },
             slot10: {
                 installation: mongoose.Schema.ObjectId,
-            },
-            slot11: {
-                installation: mongoose.Schema.ObjectId,
-            },
-            slot12: {
-                installation: mongoose.Schema.ObjectId,
-            }
-        }      
+                        }
+                },
 
-
-
+            3: {
+                slot1: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot2: {
+                    installation: mongoose.Schema.ObjectId, 
+                },
+                slot3: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot4: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot5: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot6: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot7: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot8: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot9: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot10: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot11: {
+                    installation: mongoose.Schema.ObjectId,
+                },
+                slot12: {
+                    installation: mongoose.Schema.ObjectId,
+                }
+            }      
 
     },
 
     resources: {
+        currency: Number,
+        energy: Number,
+        oxygen: Number,
+        water: Number,
+        food: Number,
+        minerals: Number,
+        darkMatter: Number
+    },
+    delta: {
         currency: Number,
         energy: Number,
         oxygen: Number,
