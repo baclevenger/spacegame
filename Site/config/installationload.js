627d13bf52d28303050b8bd1d73b372f41578124
isaacharris <isaacharris02037@gmail.com>
2015-03-18 13:49:59 -0400

    installations added to DB 3/18/15

---
+++ Site/config/installationload.js
@@ -0,0 +1,55 @@
+﻿var installation = require('./models/installation.js');
+
+installation.find(function (err, installation) {
+    if (installation.length) return;
+    
+    new installation({
+        name: "Solar Panel",
+        description: "basic production unit of energy",
+        graphic: "link",
+        cost: {
+            currency: 16,
+            energy: 5,
+            oxygen: 3,
+            water: 3,
+            food: 10,
+            minerals: 20,
+            darkMatter: 0,
+        },
+        delta: {
+            currency: 0,
+            energy: 25,
+            oxygen: 0,
+            water: 0,
+            food: 0,
+            minerals: 0,
+            darkMatter: 0
+        }
+    }).save();
+    
+    
+    
+    new installation({
+        name: "hydro plant",
+        description: "basic production unit of water",
+        graphic: "link",
+        cost: {
+            currency: 16,
+            energy: 5,
+            oxygen: 3,
+            water: 3,
+            food: 10,
+            minerals: 20,
+            darkMatter: 0,
+        },
+        delta: {
+            currency: 0,
+            energy: 25,
+            oxygen: 0,
+            water: 0,
+            food: 0,
+            minerals: 0,
+            darkMatter: 0
+        }
+    }).save();
+});
--- Site/spacegame.js
+++ Site/spacegame.js
@@ -63,6 +63,63 @@ var opts = {
 
 var user = require('./models/user.js');
 var Station = require('./models/Station.js');
+var installation = require('./models/installation.js');
+
+//installations==============================================================
+
+installation.find(function (err, installations) {
+    if (installations.length) return;
+    
+    new installation({
+        name: "Solar Panel",
+        description: "basic production unit of energy",
+        graphic: "link",
+        cost: {
+            currency: 16,
+            energy: 5,
+            oxygen: 3,
+            water: 3,
+            food: 10,
+            minerals: 20,
+            darkMatter: 0,
+        },
+        delta: {
+            currency: 0,
+            energy: 25,
+            oxygen: 0,
+            water: 0,
+            food: 0,
+            minerals: 0,
+            darkMatter: 0
+        }
+    }).save();
+    
+    
+    
+    new installation({
+        name: "hydro plant",
+        description: "basic production unit of water",
+        graphic: "link",
+        cost: {
+            currency: 16,
+            energy: 5,
+            oxygen: 3,
+            water: 3,
+            food: 10,
+            minerals: 20,
+            darkMatter: 0,
+        },
+        delta: {
+            currency: 0,
+            energy: 25,
+            oxygen: 0,
+            water: 0,
+            food: 0,
+            minerals: 0,
+            darkMatter: 0
+        }
+    }).save();
+});
 
 
 
@@ -71,6 +128,7 @@ var Station = require('./models/Station.js');
 
 
 require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 
 //Launch app
 app.listen(app.get('port'), function(){
