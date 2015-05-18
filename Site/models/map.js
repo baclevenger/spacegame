b548007cc10b319aecddeca60ca300799c23b606
isaacharris <isaacharris02037@gmail.com>
2015-05-18 12:12:15 -0400

    update 5/18

---
+++ Site/models/map.js
@@ -0,0 +1,14 @@
+﻿// app/models/user.js
+// load the things we need
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// define the schema for our user model
+
+var mapSchema = mongoose.Schema({
+    sector: Number,
+    sid: mongoose.Schema.ObjectId,
+    x: Number,
+    y: Number,
+    z: Number
+});
